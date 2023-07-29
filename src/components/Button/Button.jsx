a3eb4bc0f34032416b5f4bbc8403812849175eec
dmutrevna <101204568+dmutrevna@users.noreply.github.com>
2023-07-29 11:08:48 +0200

    fix

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-
+import { toast } from 'react-toastify';
 import PropTypes from 'prop-types';
 import { ButtonLoadMore } from './Button.styled';
 
 export const Button = ({ onButtonLoadMore }) => {
+  const handleClick = () => {
+    if (onButtonLoadMore) {
+      toast.info('Loading....Please wait');
+      onButtonLoadMore();
+    }
+  };
+
   return (
-    <ButtonLoadMore onClick={onButtonLoadMore} type="button">
+    <ButtonLoadMore onClick={handleClick} type="button">
       Load More
     </ButtonLoadMore>
   );
